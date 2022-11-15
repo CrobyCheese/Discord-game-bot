caf79c6f9345380997c0f3be58a3ca05cf89e90e
CrobyCheese <118228252+CrobyCheese@users.noreply.github.com>
2022-11-14 17:24:06 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,141 @@
+def game():
+    y = 2
+    a = input("")
+    if a == str("w"):
+        y = y + 1
+
+    else:
+        if a == str("s"):
+            y = y - 1
+
+    if y == 2:
+        print("0")
+        print("1")
+        print("0")
+
+    if y == 1:
+        print("0")
+        print("0")
+        print("1")
+
+    if y == 3:
+        print("1")
+        print("0")
+        print("0")
+
+    a = input("")
+    if a == str("w"):
+        y = y + 1
+
+    else:
+        if a == str("s"):
+            y = y - 1
+
+    if y == 2:
+        print("0")
+        print("1")
+        print("0")
+
+    if y == 1:
+        print("0")
+        print("0")
+        print("1")
+
+    if y == 3:
+        print("1")
+        print("0")
+        print("0")
+
+    a = input("")
+    if a == str("w"):
+        y = y + 1
+
+    else:
+        if a == str("s"):
+            y = y - 1
+
+    if y == 2:
+        print("0")
+        print("1")
+        print("0")
+
+    if y == 1:
+        print("0")
+        print("0")
+        print("1")
+
+    if y == 3:
+        print("1")
+        print("0")
+        print("0")
+
+    a = input("")
+    if a == str("w"):
+        y = y + 1
+
+    else:
+        if a == str("s"):
+            y = y - 1
+
+    if y == 2:
+        print("0")
+        print("1")
+        print("0")
+
+    if y == 1:
+        print("0")
+        print("0")
+        print("1")
+
+    if y == 3:
+        print("1")
+        print("0")
+        print("0")
+
+    a = input("")
+    if a == str("w"):
+        y = y + 1
+
+    else:
+        if a == str("s"):
+            y = y - 1
+
+    if y == 2:
+        print("0")
+        print("1")
+        print("0")
+
+    if y == 1:
+        print("0")
+        print("0")
+        print("1")
+
+    if y == 3:
+        print("1")
+        print("0")
+        print("0")
+    a = input("")
+    if a == str("w"):
+        y = y + 1
+
+    else:
+        if a == str("s"):
+            y = y - 1
+
+    if y == 2:
+        print("0")
+        print("1")
+        print("0")
+
+    if y == 1:
+        print("0")
+        print("0")
+        print("1")
+
+    if y == 3:
+        print("1")
+        print("0")
+        print("0")
+
+
+game()
