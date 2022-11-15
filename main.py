eccbfea9c6fa375d1d91a307d84b0b19b2a2255f
CrobyCheese <118228252+CrobyCheese@users.noreply.github.com>
2022-11-14 21:14:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,141 +1,566 @@
 def game():
+    x = 2
     y = 2
+
     a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
     if a == str("w"):
         y = y + 1
 
-    else:
-        if a == str("s"):
-            y = y - 1
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
 
     if y == 2:
-        print("0")
-        print("1")
-        print("0")
-
-    if y == 1:
-        print("0")
-        print("0")
-        print("1")
+        print("___")
+        print("000")
+        print(b)
+        print("000")
 
     if y == 3:
-        print("1")
-        print("0")
-        print("0")
+        print("___")
+        print(b)
+        print("000")
+        print("000")
 
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
     a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
     if a == str("w"):
         y = y + 1
 
-    else:
-        if a == str("s"):
-            y = y - 1
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
 
     if y == 2:
-        print("0")
-        print("1")
-        print("0")
-
-    if y == 1:
-        print("0")
-        print("0")
-        print("1")
+        print("___")
+        print("000")
+        print(b)
+        print("000")
 
     if y == 3:
-        print("1")
-        print("0")
-        print("0")
+        print("___")
+        print(b)
+        print("000")
+        print("000")
 
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
     a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
     if a == str("w"):
         y = y + 1
 
-    else:
-        if a == str("s"):
-            y = y - 1
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
 
     if y == 2:
-        print("0")
-        print("1")
-        print("0")
-
-    if y == 1:
-        print("0")
-        print("0")
-        print("1")
+        print("___")
+        print("000")
+        print(b)
+        print("000")
 
     if y == 3:
-        print("1")
-        print("0")
-        print("0")
+        print("___")
+        print(b)
+        print("000")
+        print("000")
 
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
     a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
     if a == str("w"):
         y = y + 1
 
-    else:
-        if a == str("s"):
-            y = y - 1
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
 
     if y == 2:
-        print("0")
-        print("1")
-        print("0")
-
-    if y == 1:
-        print("0")
-        print("0")
-        print("1")
+        print("___")
+        print("000")
+        print(b)
+        print("000")
 
     if y == 3:
-        print("1")
-        print("0")
-        print("0")
+        print("___")
+        print(b)
+        print("000")
+        print("000")
 
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
     a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
     if a == str("w"):
         y = y + 1
 
-    else:
-        if a == str("s"):
-            y = y - 1
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
 
     if y == 2:
-        print("0")
-        print("1")
-        print("0")
-
-    if y == 1:
-        print("0")
-        print("0")
-        print("1")
+        print("___")
+        print("000")
+        print(b)
+        print("000")
 
     if y == 3:
-        print("1")
-        print("0")
-        print("0")
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
     a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
     if a == str("w"):
         y = y + 1
 
-    else:
-        if a == str("s"):
-            y = y - 1
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
 
     if y == 2:
-        print("0")
-        print("1")
-        print("0")
-
-    if y == 1:
-        print("0")
-        print("0")
-        print("1")
+        print("___")
+        print("000")
+        print(b)
+        print("000")
 
     if y == 3:
-        print("1")
-        print("0")
-        print("0")
+        print("___")
+        print(b)
+        print("000")
+        print("000")
 
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
+    a = input("")
+
+    if a == str("a"):
+        x = x + 1
+
+    if a == str("d"):
+        x = x - 1
+
+    if a == str("s"):
+        y = y - 1
+
+    if a == str("w"):
+        y = y + 1
+
+    if x == 2:
+        b = str("010")
+
+    if x == 1:
+        b = str("001")
+
+    if x == 3:
+        b = str("100")
+
+    if y == 2:
+        print("___")
+        print("000")
+        print(b)
+        print("000")
+
+    if y == 3:
+        print("___")
+        print(b)
+        print("000")
+        print("000")
+
+    if y == 1:
+        print("___")
+        print("000")
+        print("000")
+        print(b)
 
 game()
